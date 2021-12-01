d5237e2d5c1331053497ce5d45eae5ce301723b2
Damian <a.damian@gmx.com>
2021-12-01 14:06:29 +0100

    Final commit, answers for the first and second part.

--- 2021 - Python/1_Sonar_Sweep/main.py
+++ 2021 - Python/1_Sonar_Sweep/main.py
@@ -1,53 +1,25 @@
-"""
-As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
-
-For example, suppose you had the following report:
-
-199
-200
-208
-210
-200
-207
-240
-269
-260
-263
-This report indicates that, scanning outward from the submarine, the sonar sweep found depths of 199, 200, 208, 210, and so on.
-
-The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
-
-To do this, count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.) In the example above, the changes are as follows:
-
-199 (N/A - no previous measurement)
-200 (increased)
-208 (increased)
-210 (increased)
-200 (decreased)
-207 (increased)
-240 (increased)
-269 (increased)
-260 (decreased)
-263 (increased)
-In this example, there are 7 measurements that are larger than the previous measurement.
-
-How many measurements are larger than the previous measurement?
-"""
-
-
-with open('input.txt', 'r') as f:
-    data = f.read()
-measurements = [int(x) for x in data.splitlines()]
-
-
-def count_increments(data):
+def count_increments(input_data):
+    """
+    Takes a list of values and calculates how many increments between consecutive values there are.
+    :return: List
+    """
     increments = 0
-    previous_line = data[0]
-    for n in data:
+    previous_line = input_data[0]
+    for n in input_data:
         if n > previous_line:
             increments += 1
         previous_line = n
     return increments
 
 
-print(count_increments(measurements))
+with open('input.txt', 'r') as f:
+    input_data = f.read()
+
+
+measurements = [int(x) for x in input_data.splitlines()]
+summed_windows = [sum(x) for x in zip(measurements, measurements[1:], measurements[2:])]
+
+# Answer for the first part:
+print(f"Answer for the first part: {count_increments(measurements)}")
+# Answer for the second part:
+print(f"Answer for the second part: {count_increments(summed_windows)}")
