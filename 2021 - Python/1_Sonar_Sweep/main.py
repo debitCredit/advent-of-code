1e3fdf915df5e35ef888d78bbe507bab86f4f466
Damian <a.damian@gmx.com>
2021-12-01 18:44:01 +0100

    refactor to a more elegant solution

--- 2021 - Python/1_Sonar_Sweep/main.py
+++ 2021 - Python/1_Sonar_Sweep/main.py
@@ -1,25 +1,17 @@
-def count_increments(input_data):
-    """
-    Takes a list of values and calculates how many increments between consecutive values there are.
-    :return: List
-    """
-    increments = 0
-    previous_line = input_data[0]
-    for n in input_data:
-        if n > previous_line:
-            increments += 1
-        previous_line = n
-    return increments
-
-
 with open('input.txt', 'r') as f:
     input_data = f.read()
 
+measurements = list(map(int, input_data.splitlines()))
 
-measurements = [int(x) for x in input_data.splitlines()]
-summed_windows = [sum(x) for x in zip(measurements, measurements[1:], measurements[2:])]
 
-# Answer for the first part:
-print(f"Answer for the first part: {count_increments(measurements)}")
-# Answer for the second part:
-print(f"Answer for the second part: {count_increments(summed_windows)}")
+def part1(data: list) -> int:
+    return sum(b > a for a, b in zip(data, data[1:]))
+
+
+# This works because a + b + c < b + c + d can be reduced to a < b
+def part2(data: list) -> int:
+    return sum(b > a for a, b in zip(data, data[3:]))
+
+
+print(part1(measurements))
+print(part2(measurements))
