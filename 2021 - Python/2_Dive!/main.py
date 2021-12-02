6648ce33934affb02d844f426732c71ecd339d86
Damian <a.damian@gmx.com>
2021-12-02 19:22:05 +0100

    naive solutions for part 1 and part 2

--- 2021 - Python/2_Dive!/main.py
+++ 2021 - Python/2_Dive!/main.py
@@ -1,31 +1,31 @@
-a = {}
 with open('input.txt', 'r') as f:
     input_data = f.read()
     a = input_data.splitlines()
 
 horizontal, depth, aim = 0, 0, 0
 
-# for i in a:
-#     c, v = i.split()
-#     if c == "forward":
-#         forward += int(v)
-#     elif c == "down":
-#         depth += int(v)
-#     elif c == "up":
-#         depth -= int(v)
-
-# print(forward * depth)  # 1938402
-
-# TODO: Submission fails - to be debugged.
+# Part 1
 for i in a:
     c, v = i.split()
     if c == "forward":
         horizontal += int(v)
-        depth += (depth * aim)
-        print(f"{aim=}, {depth=}, {horizontal=}")
     elif c == "down":
-        aim += int(v)
+        depth += int(v)
     elif c == "up":
-        aim -= int(v)
+        depth -= int(v)
 
-print(horizontal * depth)
+print(horizontal * depth)  # 1938402
+
+# Part 2 (uncomment to run)
+
+# for i in a:
+#     c, v = i.split()
+#     if c == "forward":
+#         horizontal += int(v)
+#         depth += (int(v) * aim)
+#     elif c == "down":
+#         aim += int(v)
+#     elif c == "up":
+#         aim -= int(v)
+#
+# print(horizontal * depth)  # 1947878632
