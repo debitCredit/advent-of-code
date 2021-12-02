f5f1a56508b4a32ad199630076f45613605c0830
Damian <a.damian@gmx.com>
2021-12-02 17:15:11 +0100

    started work on part2

--- 2021 - Python/2_Dive!/main.py
+++ 2021 - Python/2_Dive!/main.py
@@ -3,15 +3,29 @@ with open('input.txt', 'r') as f:
     input_data = f.read()
     a = input_data.splitlines()
 
-forward, depth = 0, 0
+horizontal, depth, aim = 0, 0, 0
 
+# for i in a:
+#     c, v = i.split()
+#     if c == "forward":
+#         forward += int(v)
+#     elif c == "down":
+#         depth += int(v)
+#     elif c == "up":
+#         depth -= int(v)
+
+# print(forward * depth)  # 1938402
+
+# TODO: Submission fails - to be debugged.
 for i in a:
     c, v = i.split()
     if c == "forward":
-        forward += int(v)
+        horizontal += int(v)
+        depth += (depth * aim)
+        print(f"{aim=}, {depth=}, {horizontal=}")
     elif c == "down":
-        depth += int(v)
+        aim += int(v)
     elif c == "up":
-        depth -= int(v)
+        aim -= int(v)
 
-print(forward * depth)  # 1938402
+print(horizontal * depth)
