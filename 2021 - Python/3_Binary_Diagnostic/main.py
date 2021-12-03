a37d3152de0bae40054fa8b2ee2ed7e72520ea12
Damian <a.damian@gmx.com>
2021-12-03 16:51:36 +0100

    small refactoring

--- 2021 - Python/3_Binary_Diagnostic/main.py
+++ 2021 - Python/3_Binary_Diagnostic/main.py
@@ -3,7 +3,7 @@ from collections import Counter
 with open('input.txt') as f:
     input_data = f.read()
 
-diagnostic = list(input_data.splitlines())
+diag = list(input_data.splitlines())
 
 
 def extract_nth(lst: list, pos: int) -> list:
@@ -14,17 +14,17 @@ a = []
 gamma_rate_bin = []
 
 for i in range(12):
-    a.insert(i, extract_nth(diagnostic, i))
+    a.insert(i, extract_nth(diag, i))
 
 for n in a:
     for k, v in (Counter(n).most_common(1)):
         gamma_rate_bin.append(k)
 
-gamma_rate_string = "".join(gamma_rate_bin)
+g_rate = "".join(gamma_rate_bin)
 
 e_rate = "".join(["1" if x == "0" else "0" for x in gamma_rate_bin])
 
-print(f"Part1: {int(e_rate, 2) * int(gamma_rate_string, 2)}")  # 4006064
+print(f"Part1: {int(e_rate, 2) * int(g_rate, 2)}")  # 4006064
 
 
 # Part 2
@@ -38,13 +38,13 @@ def calc_common(lst: list, oxygen=True, most=True) -> str:
     return r[0][0] if most else r[1][0]
 
 
-a = diagnostic
+a, u = diag, diag
 for n in range(12):
     if len(a) > 1:
         b = extract_nth(a, n)
         a = [i for i in a if calc_common(b) == i[n]]
 
-u = diagnostic
+
 for n in range(12):
     if len(u) > 1:
         b = extract_nth(u, n)
@@ -52,4 +52,3 @@ for n in range(12):
 
 
 print(f"Part2: {int(*a, 2) * int(*u, 2)}")
-
