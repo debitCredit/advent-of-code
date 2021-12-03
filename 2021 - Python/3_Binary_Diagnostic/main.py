33532151e1c4c8f82105acfbfebdc8606743e60a
Damian <a.damian@gmx.com>
2021-12-03 15:17:59 +0100

    added two helper funcs for part 2

--- 2021 - Python/3_Binary_Diagnostic/main.py
+++ 2021 - Python/3_Binary_Diagnostic/main.py
@@ -25,10 +25,26 @@ gamma_rate_string = "".join(gamma_rate_bin)
 epsilon_rate_bin = ["1" if x == "0" else "0" for x in gamma_rate_bin]
 epsilon_rate_string = "".join(epsilon_rate_bin)
 
-# Part 1
-print(f"Part1: {int(epsilon_rate_string, 2) * int(gamma_rate_string, 2)}")
+print(f"Part1: {int(epsilon_rate_string, 2) * int(gamma_rate_string, 2)}")  # 4006064
 
 # Part 2
+
+# extract nth elements of the lists in list of lists
+
+
+def extract_nth(lst: list, pos: int) -> list:
+    return [i[pos] for i in lst]
+
+
+def calc_most_common(lst: list, oxygen=True) -> str:
+    r = Counter(lst).most_common(2)
+    if r[0][1] == r[1][1] and oxygen:
+        return "1"
+    elif r[0][1] == r[1][1] and not oxygen:
+        return "0"
+    return r[0][0]
+
+
 
 
 
