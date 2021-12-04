e3131ed5f657cbed7dbb90eecf245307834cddef
Damian <a.damian@gmx.com>
2021-12-04 19:17:28 +0100

    solution for part 1

--- 2021 - Python/4_Giant_Squid/main.py
+++ 2021 - Python/4_Giant_Squid/main.py
@@ -1,9 +1,8 @@
 import numpy as np
-import numpy.ma as ma
 
 boards = []
 with open('input.txt') as f:
-    draw = list(next(f).strip().split(","))
+    draw = list(map(int, next(f).strip().split(",")))
     counter = 0
     for line in f:
         if not line == "\n" and counter == 0:
@@ -19,4 +18,37 @@ with open('input.txt') as f:
         else:
             counter = 0
 
-print(boards)
+
+def check_if_won(arr: np.ndarray) -> bool:
+    for q in arr:
+        if np.all(q):
+            return True
+    for e in range(5):
+        if np.all(arr[:, e]):
+            return True
+    if np.all(arr.diagonal()) or np.all(np.fliplr(arr).diagonal()):
+        return True
+    return False
+
+
+def process_boards(draw: list, boards: list) -> tuple:
+    scores = {}
+    scores_so_far = []
+    for d in draw:
+        scores_so_far.append(d)
+        for i in range(len(boards)):
+            scores[d] = np.asarray(np.in1d(boards[i], scores_so_far)).reshape((5, 5))
+            if check_if_won(scores[d]):
+                return d, scores[d], i
+
+
+d, m, b = process_boards(draw, boards)
+
+sum_of_false = 0
+
+for j in range(5):
+    for i in range(5):
+        if not m[j][i]:
+            sum_of_false += boards[b][j][i]
+
+print(f"Part 1: {sum_of_false * d}")  # 21607
