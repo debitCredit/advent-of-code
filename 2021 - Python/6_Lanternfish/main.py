8a4b7335f3576102d618375165bc3ad3ff1a79d3
Damian <debitCredit@users.noreply.github.com>
2021-12-06 12:19:47 +0100

    solved part 1

--- 2021 - Python/6_Lanternfish/main.py
+++ 2021 - Python/6_Lanternfish/main.py
@@ -1,7 +1,24 @@
-# TODO: read first line and enumerate
-# Read all lines, perform check each element, if 0 keep counter and add required number of new fish
+import numpy as np
 
-with open('input.txt') as f:
-    draw = list(map(int, next(f).strip().split(",")))
+file = "input.txt"
+days = 80
+# file = "test_input.txt"
+# days = 18
 
-print(draw)
+with open(file) as f:
+    fish = np.array(list(map(int, next(f).strip().split(","))))
+
+
+def process_day(fish: np.array, days=days) -> np.ndarray:
+    for d in range(days):
+        new_fish_count = np.sum(fish == 0)
+        fish[fish == 0] = 10
+        fish = np.where(fish == 0, fish, fish - 1)
+        fish[fish == 9] = 6
+        fish = np.append(fish, np.repeat(8, new_fish_count))
+    return fish
+
+
+answer1 = print(f"Part 1: {process_day(fish).size}")
+
+
