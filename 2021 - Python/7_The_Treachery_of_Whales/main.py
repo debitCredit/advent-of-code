b67491828c5c1d2a6df825dd133a8190f0bccafc
Damian <a.damian@gmx.com>
2021-12-07 16:08:13 +0100

    refactor of part 1 and solved for part 2

--- 2021 - Python/7_The_Treachery_of_Whales/main.py
+++ 2021 - Python/7_The_Treachery_of_Whales/main.py
@@ -1,20 +1,14 @@
-import numpy as np
+import statistics
 
 file = "input.txt"
-# file = "test_input.txt"
 
 with open(file) as f:
     data = list(map(int, next(f).strip().split(",")))
 
+median, mean = int(statistics.median(data)), int(statistics.mean(data))
 
-def min_cost(d: list) -> int:
-    data.sort()
-    median_crab = int(np.median(data))
-    cost = 0
-    len_data = len(data)
-    for i in range(len_data):
-        cost += abs(data[i] - median_crab)
-    return cost
+print(f"Part 1: {sum(abs(c - median) for c in data)}")
 
+gauss = lambda v: v * (v + 1) // 2
 
-print(f"Part 1: {min_cost(data)}")
+print(f"Part 2: {sum(gauss(abs(c - mean)) for c in data)}")
