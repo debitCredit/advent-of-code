b5e52af286528a4436d5aff43e908508b73ad212
Damian <a.damian@gmx.com>
2021-12-07 10:08:59 +0100

    solved part 1

--- 2021 - Python/7_The_Treachery_of_Whales/main.py
+++ 2021 - Python/7_The_Treachery_of_Whales/main.py
@@ -1,6 +1,20 @@
-import scipy
+import numpy as np
 
 file = "input.txt"
+# file = "test_input.txt"
 
 with open(file) as f:
     data = list(map(int, next(f).strip().split(",")))
+
+
+def min_cost(d: list) -> int:
+    data.sort()
+    median_crab = int(np.median(data))
+    cost = 0
+    len_data = len(data)
+    for i in range(len_data):
+        cost += abs(data[i] - median_crab)
+    return cost
+
+
+print(f"Part 1: {min_cost(data)}")
