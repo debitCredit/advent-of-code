6f0cc7478baa5610078f067974608b4e3cc9a3b5
dank <a.damian@gmx.com>
2022-05-15 22:31:35 +0200

    day 10 part 2

--- 2021 - Python/10_Syntax_Scoring/main.py
+++ 2021 - Python/10_Syntax_Scoring/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 file = "input.txt"
 # file = "test_input.txt"
 
@@ -9,15 +11,33 @@ entry = list(input_data.splitlines())
 stack = []
 opposites = {")": "(", "]": "[", ">": "<", "}": "{"}
 score_list = {")": 3, "]": 57, "}": 1197, ">": 25137}
+score_list2 = {"(": 1, "[": 2, "{": 3, "<": 4}
 total = 0
-
+scores = []
+count = 0
 # Part 1
+# for line in entry:
+#     for i in line:
+#         if i in '([{<':
+#             stack.append(i)
+#         elif stack.pop() != opposites[i]:
+#             total += score_list[i]
+
+# Part 2
+
 for line in entry:
     for i in line:
-
         if i in '([{<':
             stack.append(i)
         elif stack.pop() != opposites[i]:
-            total += score_list[i]
+            stack = []
+            break
+    else:
+        score = 0
+        while stack:
+            score = score * 5 + score_list2[stack.pop()]
+        scores.append(score)
+        continue
 
-print(total)
+print(int(np.median(scores)))
+
