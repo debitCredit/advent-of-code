37a7ff74e22782831ca95bf4f337f657a656435c
Damian <debitCredit@users.noreply.github.com>
2022-05-24 13:49:29 +0200

    started on day 12

---
+++ 2021 - Python/12_Passage_Pathing/main.py
@@ -0,0 +1,12 @@
+from pyaoc import *
+# from itertools import count
+
+file = "input.txt"
+# file = "test_input.txt"
+
+with open(file) as f:
+    input_data = f.read()
+
+entry = list(input_data.splitlines())
+
+print(entry)
