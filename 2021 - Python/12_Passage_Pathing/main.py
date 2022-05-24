84729db990449a4a74f2bcc82e86d56ee8492b2c
Damian <debitCredit@users.noreply.github.com>
2022-05-24 22:03:48 +0200

    started on day 12

--- 2021 - Python/12_Passage_Pathing/main.py
+++ 2021 - Python/12_Passage_Pathing/main.py
@@ -1,12 +1,31 @@
-from pyaoc import *
-# from itertools import count
+from collections import defaultdict
 
 file = "input.txt"
 # file = "test_input.txt"
 
 with open(file) as f:
-    input_data = f.read()
+    input_data = f.readlines()
 
-entry = list(input_data.splitlines())
+cave_network = defaultdict(list)
 
-print(entry)
+for line in input_data:
+    u, v = line.strip().split("-")
+    cave_network[u].append(v)
+    cave_network[v].append(u)
+
+
+def dfs(caves, cave, seen, part2=False):
+    if cave == "end":
+        return 1
+    ans = 0
+    for c in caves[cave]:
+        if c not in seen:
+            tmp = {c} if c.islower() else set()
+            ans += dfs(caves, c, seen | tmp, part2)
+        elif part2 and c != "start":
+            ans += dfs(caves, c, seen, False)
+    return ans
+
+
+print(dfs(cave_network, "start", {"start"}))
+print(dfs(cave_network, "start", {"start"}, True))
