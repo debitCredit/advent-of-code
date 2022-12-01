5f7966c30234304de1e49440176979afd02f5c7a
Damian <debitCredit@users.noreply.github.com>
2022-12-01 23:20:45 +0100

    solved day 1

---
+++ 2022 - Python/01_Calorie_Counting/part2.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from collections import defaultdict
+
+
+file = os.path.join(sys.path[0], "input.txt")
+# file = os.path.join(sys.path[0], "test_input.txt")
+
+ll = [[int(y) for y in x.split('\n')] for x in open(file).read().strip().split('\n\n')]
+
+print(sum(sorted(sum(x) for x in ll)[-3:]))
