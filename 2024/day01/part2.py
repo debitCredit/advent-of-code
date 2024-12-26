44fbcbcc9aa2f25189bf5065822779b6036b3f6b
kafka <kafka@ptr-8rf3vrawql34pwn5h1m.18120a2.ip6.access.telenet.be>
2024-12-26 22:55:52 +0100

    Removed code for first part.

--- 2024/day01/part2.py
+++ 2024/day01/part2.py
@@ -10,13 +10,6 @@ with open(file) as f:
     lst1.append(d1)
     lst2.append(d2)
 
-lst1.sort()
-lst2.sort()
-
-total_part1 = sum(abs(n1 - n2) for n1, n2 in zip(lst1, lst2))
-
-print(total_part1)
-
 running_total_second = 0
 count_second = Counter(lst2)
 
