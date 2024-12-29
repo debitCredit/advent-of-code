ba78637d4a492ebf47e8aafb464a6e69fe1c6fca
kafka <a.damian@gmx.com>
2024-12-29 14:07:14 +0100

    Solved day04 part2

--- 2024/day04/part2.py
+++ 2024/day04/part2.py
@@ -2,8 +2,18 @@
 file = "input.txt"
 
 with open(file) as f:
-  for line in f:
-    pass
+  grid = f.read().splitlines()
 
+count = 0
 
-# print(total_part1)
+for row in range(len(grid)):
+  for col in range(len(grid[0])):
+    if grid[row][col] != "A": continue
+    # Check if out of bounds
+    if row - 1 < 0 or row + 1 > len(grid) -1: continue
+    if col - 1 < 0 or col + 1 > len(grid[0]) -1: continue
+
+    if {grid[row - 1][col - 1], grid[row + 1][col + 1]} == {"M", "S"} and {grid[row + 1][col - 1], grid[row - 1][col + 1]} == {"M", "S"}:
+      count += 1
+
+print(count)
