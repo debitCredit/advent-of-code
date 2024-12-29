a49b3c58d046581f94ad08d3015a904379e79659
kafka <a.damian@gmx.com>
2024-12-29 11:55:55 +0100

    Refactored day03 part1

--- 2024/day03/part1.py
+++ 2024/day03/part1.py
@@ -1,13 +1,14 @@
 import re
 
-# file = "test_input.txt"
-file = "input.txt"
+file = "test_input.txt"
+# file = "input.txt"
 
 with open(file) as f:
-  text= f.read()
+    text = f.read()
 
-pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
-number_pairs = re.findall(pattern, text)
-number_pairs = [(int(x), int(y)) for x, y in number_pairs]
+total = sum(
+    int(x) * int(y) 
+    for x, y in re.findall(r'mul\((\d+),(\d+)\)', text)
+)
 
-print(sum(pair[0] * pair[1] for pair in number_pairs))
+print(total)
