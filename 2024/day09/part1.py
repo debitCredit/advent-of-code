91d5b256a145fbd4010d8b8ebda68350d76a0182
kafka <a.damian@gmx.com>
2025-01-04 15:50:19 +0100

    Solved day09 part1

---
+++ 2024/day09/part1.py
@@ -0,0 +1,46 @@
+from itertools import cycle
+
+
+# file = "test_input.txt"
+file = "input.txt"
+
+with open(file) as f:
+  disk_map = [int(x) for x in list(f.read().strip())]
+
+
+def unpack(disk_map: list) -> list:
+  data_type = cycle(["file", "free_space"])
+  unpacked = []
+  data_index = 0
+  for i, data in enumerate(disk_map):
+    dtype = next(data_type)
+    if dtype == "file":
+      unpacked.extend([data_index] * data)
+      data_index += 1
+    else:
+      unpacked.extend(["."] * data)
+  return unpacked
+
+
+def compact(unpacked: list) -> list:
+  for i in range(len(unpacked)):
+    if unpacked[i] == ".":
+      remaining = unpacked[i+1:]
+      rev_index, value = next(((i, x) for i, x in enumerate(reversed(remaining)) if x != "."), (None, None))
+      if rev_index is not None:
+          original_index = len(unpacked) - 1 - rev_index
+          unpacked[i], unpacked[original_index] = unpacked[original_index], unpacked[i]
+  return unpacked
+
+
+def checksum_calc(compacted: list) -> int:
+  checksum = 0
+  for i, data in enumerate(compacted):
+    if compacted[i] == ".":
+      return checksum
+    else:
+      checksum += i * data
+  return checksum
+
+
+print(checksum_calc(compact(unpack(disk_map))))
