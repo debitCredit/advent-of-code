247428ca635c55eb14c6cb02bfd76b5e0d2fc79b
kafka <a.damian@gmx.com>
2025-01-05 17:00:36 +0100

    Solved day11 part2

--- 2024/day11/part1.py
+++ 2024/day11/part1.py
@@ -1,24 +1,25 @@
-# file = "test_input.txt"
-file = "input.txt"
+from itertools import chain
+
+file = "test_input.txt"
+# file = "input.txt"
 
 with open(file) as f:
   stones = [int(x) for x in f.read().split()]
 
 
-def blink(stones: list, blinks_count: int) -> list:
-  for _ in range(blinks_count):
-    new_stones = []
-    for stone in stones:
-      if stone == 0:
-        new_stones.append(1)
-      elif len(str(abs(stone))) % 2 == 0:
-        stone_str = str(stone)
-        first, second = stone_str[:len(stone_str)//2], stone_str[len(stone_str)//2:]
-        new_stones.extend([int(first), int(second)])
-      else:
-        new_stones.append(stone * 2024)
-    stones = new_stones
-  return(new_stones)
+def step_stone(s: int) -> list[int]:
+  if s == 0:
+    return [1]
+  
+  if len(str(s)) % 2 == 0:
+    stone_str = str(s)
+    first, second = stone_str[:len(stone_str)//2], stone_str[len(stone_str)//2:]
+    return [int(first), int(second)]
+  
+  return [s * 2024]
+
+for _ in range(25):
+  stones = chain.from_iterable(map(step_stone, stones))
 
 
-print(len(blink(stones, 25)))
+print(len(list(stones)))
--- 2024/day11/part2.py
+++ 2024/day11/part2.py
@@ -1,9 +1,33 @@
-file = "test_input.txt"
-# file = "input.txt"
+from collections import defaultdict
+
+# file = "test_input.txt"
+file = "input.txt"
 
 with open(file) as f:
-  for line in f:
-    pass
+    stones = [int(x) for x in f.read().split()]
+
+def step_stone(s: int) -> list[int]:
+    if s == 0:
+        return [1]
+    
+    if len(str(s)) % 2 == 0:
+        stone_str = str(s)
+        first, second = stone_str[:len(stone_str)//2], stone_str[len(stone_str)//2:]
+        return [int(first), int(second)]
+    
+    return [s * 2024]
 
 
-# print(total_part1)
+stones_count = defaultdict(int)
+for stone in stones:
+    stones_count[stone] += 1
+
+
+for i in range(75):
+    new_stones = defaultdict(int)
+    for stone, count in stones_count.items():
+        for new_stone in step_stone(stone):
+            new_stones[new_stone] += count
+    stones_count = new_stones
+
+print(sum(stones_count.values()))
