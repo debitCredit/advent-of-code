a791846826503e207678ebdefe761ed3144f8a80
kafka <a.damian@gmx.com>
2025-01-06 13:12:25 +0100

    Solved day12 part1

--- 2024/day12/part1.py
+++ 2024/day12/part1.py
@@ -1,9 +1,54 @@
 from utils.utils import neighbors, parse_grid, GridPoint, Grid
 
-file = "test_input.txt"
-# file = "input.txt"
+# file = "test_input.txt"
+file = "input.txt"
 
 with open(file) as f:
-  grid = parse_grid(f.read())
+  grid = parse_grid(f.read().splitlines())
 
-print(grid[0, 0])
+
+def find_regions(grid: Grid) -> list[set[GridPoint]]:
+  regions: list[set[GridPoint]] = []
+  visited: set[GridPoint] = set()
+
+  for point in grid:
+    if point in visited:
+      continue
+    
+    # Identified new region
+    region = set()
+    stack = [point]
+
+    while stack:
+      curr = stack.pop()
+      if curr in region:
+        continue
+
+      region.add(curr)
+      curr_value = grid[curr]
+
+      stack.extend(
+          n for n in neighbors(center=curr, num_dirs=4)
+          if grid.get(n) == curr_value and n not in region
+      )
+
+    visited |= region
+    regions.append(region)
+
+  return regions
+
+
+def determine_perimiter(point: GridPoint) -> int:
+  return 4 - len([n for n in neighbors(center=point, num_dirs=4) if grid.get(n) == grid.get(point)])
+
+
+regions = find_regions(grid)
+
+total = 0
+for region in regions:
+  perimeter = 0
+  for point in region:
+    perimeter += determine_perimiter(point)
+  total += perimeter * len(region)
+
+print(total)
